fb8bb8cbcbc41b6cc95fca88953aac0acc65dac0
blk <a.belolipec@ya.ru>
2019-03-03 13:55:12 +0300

    Optimization

--- src/main/java/src/FileLoader.java
+++ src/main/java/src/FileLoader.java
@@ -8,14 +8,15 @@ class FileLoader {
     ArrayList<String> getListByFileName(String fileName) {
         ArrayList<String> linesFromFile = new ArrayList<>();
         InputStreamReader fileReader;
-        File myFile = new File(fileName);
         try {
-            fileReader = new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8);
+            fileReader = new InputStreamReader(new FileInputStream(fileName),
+                    StandardCharsets.UTF_8);
         } catch (FileNotFoundException ex) {
             return linesFromFile;
         }
         BufferedReader reader = new BufferedReader(fileReader);
         String line;
+
         try {
             while ((line = reader.readLine()) != null) {
                 if (!line.isEmpty())
@@ -24,6 +25,7 @@ class FileLoader {
         } catch (IOException ex) {
             return linesFromFile;
         }
+
         try {
             reader.close();
         } catch (IOException e) {
